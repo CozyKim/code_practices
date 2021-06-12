c088f048d185e5f6c6ce3418cfb94270e3de2a97
jaehyun <robot032195@gmail.com>
2021-06-12 17:45:13 +0900

    210612

--- Programmers_CodeTest/lv2_morespicy.py
+++ Programmers_CodeTest/lv2_morespicy.py
@@ -1,27 +1,32 @@
 # https://programmers.co.kr/learn/courses/30/lessons/42626
-def solution(scoville: list, K: int):
+import heapq
+
+
+def solution(scoville, K):
     answer = 0
-    scoville.sort(reverse=True)
-    if scoville[-1] + scoville[-2] == 0:
-        answer = -1
-    else:
-        while len(scoville) > 1:
-            min1 = scoville.pop()
-            min2 = scoville.pop()
-            if min1 >= K:
-                return answer
-            else:
-                scoville.append(make_new_scoville(min1, min2))
-                scoville.sort(reverse=True)
-                answer += 1
-        if scoville[0] >= K:
+    heapq.heapify(scoville)
+    min1 = heapq.heappop(scoville)
+    min2 = heapq.heappop(scoville)
+
+    while len(scoville) > 0:
+        if min1 >= K:
             return answer
         else:
-            return -1
+            min1 = heapq.heappushpop(
+                scoville, make_new_scoville(min1, min2))
+            if len(scoville) > 0:
+                min2 = heapq.heappop(scoville)
+            answer += 1
+    if min1 >= K:
+        return answer
+    elif make_new_scoville(min1, min2) >= K:
+        return answer + 1
+    else:
+        return -1
 
 
 def make_new_scoville(a, b):
     return a + b * 2
 
 
-print(solution([1, 2, 3, 9, 10, 12], 7))
+print(solution([1, 2, 3], 6))
