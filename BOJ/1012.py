ded6f2dd3fadf6efaeda946f70d0f4c208428868
jaehyun <robot032195@gmail.com>
2021-06-13 01:33:08 +0900

    210613

--- BOJ/1012.py
+++ BOJ/1012.py
@@ -1,73 +1,37 @@
-# """
-# 차세대 영농인 한나는 강원도 고랭지에서 유기농 배추를 재배하기로 하였다. 농약을 쓰지 않고 배추를 재배하려면 배추를 해충으로부터 보호하는 것이 중요하기 때문에, 한나는 해충 방지에 효과적인 배추흰지렁이를 구입하기로 결심한다. 이 지렁이는 배추근처에 서식하며 해충을 잡아 먹음으로써 배추를 보호한다. 특히, 어떤 배추에 배추흰지렁이가 한 마리라도 살고 있으면 이 지렁이는 인접한 다른 배추로 이동할 수 있어, 그 배추들 역시 해충으로부터 보호받을 수 있다.
-
-# (한 배추의 상하좌우 네 방향에 다른 배추가 위치한 경우에 서로 인접해있다고 간주한다)
-
-# 한나가 배추를 재배하는 땅은 고르지 못해서 배추를 군데군데 심어놓았다. 배추들이 모여있는 곳에는 배추흰지렁이가 한 마리만 있으면 되므로 서로 인접해있는 배추들이 몇 군데에 퍼져있는지 조사하면 총 몇 마리의 지렁이가 필요한지 알 수 있다.
-
-# 예를 들어 배추밭이 아래와 같이 구성되어 있으면 최소 5마리의 배추흰지렁이가 필요하다.
-
-# (0은 배추가 심어져 있지 않은 땅이고, 1은 배추가 심어져 있는 땅을 나타낸다.)
-
-# 1	1	0	0	0	0	0	0	0	0
-# 0	1	0	0	0	0	0	0	0	0
-# 0	0	0	0	1	0	0	0	0	0
-# 0	0	0	0	1	0	0	0	0	0
-# 0	0	1	1	0	0	0	1	1	1
-# 0	0	0	0	1	0	0	1	1	1
-# """
-# """
-# input
-# 입력의 첫 줄에는 테스트 케이스의 개수 T가 주어진다.
-# 그 다음 줄부터 각각의 테스트 케이스에 대해 첫째 줄에는 배추를 심은 배추밭의 가로길이 M(1 ≤ M ≤ 50)과
-# 세로길이 N(1 ≤ N ≤ 50), 그리고 배추가 심어져 있는 위치의 개수 K(1 ≤ K ≤ 2500)이 주어진다.
-# 그 다음 K줄에는 배추의 위치 X(0 ≤ X ≤ M-1), Y(0 ≤ Y ≤ N-1)가 주어진다.
-
-# output
-# 각 테스트 케이스에 대해 필요한 최소의 배추흰지렁이 마리 수를 출력한다.
-# """
-
-
-import sys
 from collections import deque
+import sys
+input = sys.stdin.readline
 
+for _ in range(int(input())):
+    m, n, k = map(int, input().split())
 
-def check(positions: list):
-    cnt = 0
-    positions.sort()
-    # print(positions)
+    lis = [[0]*m for i in range(n)]
+    for _ in range(k):
+        x, y = map(int, input().split())
+        lis[y][x] = 1
+    count = 0
 
-    while positions:
-        visited = []
-        stack = [positions.pop()]
-        cnt += 1
+    def dfs(lis, i, j):
+        dx = [0, 0, 1, -1]
+        dy = [1, -1, 0, 0]  # 우, 좌, 하, 상
+        stack = [[i, j]]
 
         while stack:
-            n = stack.pop()
-            if n not in visited:
-                visited.append(n)
+            a, b = stack.pop()
+            lis[a][b] = -1
+            for i in range(4):
+                x = a + dx[i]
+                y = b + dy[i]
+                if 0 <= x < n and 0 <= y < m and lis[x][y] == 1:
+                    lis[x][y] == -1
+                    stack.append([x, y])
 
-                if [n[0]+1, n[1]] in positions:
-                    stack.append([n[0]+1, n[1]])
-                    positions.remove([n[0]+1, n[1]])
-                if [n[0], n[1]+1] in positions:
-                    stack.append([n[0], n[1]+1])
-                    positions.remove([n[0], n[1]+1])
-                if [n[0], n[1]-1] in positions:
-                    stack.append([n[0], n[1]-1])
-                    positions.remove([n[0], n[1]-1])
-                if [n[0]-1, n[1]] in positions:
-                    stack.append([n[0]-1, n[1]])
-                    positions.remove([n[0]-1, n[1]])
+    for i in range(n):
+        for j in range(m):
+            if lis[i][j] <= 0:
+                lis[i][j] = -1
+            else:
+                count += 1
+                dfs(lis, i, j)
 
-    return cnt
-
-
-T = int(input())
-for _ in range(T):
-    M, N, K = map(int, input().split())
-    positions = []
-    for _ in range(K):
-        positions.append(list(map(int, sys.stdin.readline().split())))
-    # print(positions)
-    print(check(positions))
+    print(count)
