6a202f2898120c240b7818df1f09241164a9f9b4
jaehyun <robot032195@gmail.com>
2021-06-16 00:39:02 +0900

    210616

--- Programmers_CodeTest/lv2_maximumnumber.py
+++ Programmers_CodeTest/lv2_maximumnumber.py
@@ -1,43 +1,9 @@
-
-
-def solution(numbers):
-    answer = ''
-    _numbers = {}
-    tmp3 = []
-    a = list(map(str, numbers))
-
-    for i in a:
-        if i[0] not in _numbers:
-            _numbers[i[0]] = [i]
-        else:
-            _numbers[i[0]].append(i)
-
-    for n in _numbers:
-        n_numbers = _numbers[n]
-        tmp = []
-        for i in n_numbers:
-            tmp.append([i + n*(3-len(i)), i])
-        tmp.sort()
-        tmp2 = [tmp.pop()[1]]
-        while tmp:
-            extend_num, num = tmp.pop()
-            if len(tmp) != 0:
-                if tmp2[-1] == extend_num:
-                    if extend_num[0] < extend_num[1]:
-                        tmp2.insert(tmp2.index(tmp2[-1]), num)
-                    else:
-                        tmp2.append(num)
-                else:
-                    tmp2.append(num)
-            else:
-                tmp2.append(num)
-            print(tmp2)
-        tmp3.append(''.join(tmp2))
-        print(tmp3)
-    tmp3.sort(reverse=True)
-    print(tmp3)
-    print(''.join(tmp3))
-    return ''.join(tmp3)
+# https://programmers.co.kr/learn/courses/30/lessons/42746?language=python3
+def solution(num):
+    num = list(map(str, num))
+    num.sort(key=lambda x: x*3,
+             reverse=True)
+    return str(int(''.join(num)))
 
 
 solution([1, 0, 1, 11, 131, 1000, 2, 242, 212, 21, 24,
