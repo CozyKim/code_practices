cd7e861f0ddcf7d522f22407f0e6ae08896b7aeb
jaehyun <robot032195@gmail.com>
2021-06-22 23:13:42 +0900

    210622

--- Programmers_CodeTest/lv2_makemaxmumnum.py
+++ Programmers_CodeTest/lv2_makemaxmumnum.py
@@ -1,34 +1,18 @@
 # https://programmers.co.kr/learn/courses/30/lessons/42883?language=python3
 
 def solution(number: str, k):
-    global answer
-    answer = ''
 
-    for i in '9876543210':
-        if i in number:
-            if number.index(i) <= k:
-                k -= number.index(i)
-                number = number[number.index(i):]
-                break
+    stack = [number[0]]
 
-    def opt(answers, k, last):
-        global answer
-        if k == 0:
-            answer += answers
-            return
-        for idx, i in enumerate(answers):
-            if last < i:
-                answers = answers[:idx-1]+answers[idx:]
-                opt(answers, k - 1, answers[0])
-                return
-            if idx == len(answers) - 1 and k != 0:
-                answer += answers[:idx+1 - k]
-                return
-            last = i
-    opt(number, k, number[0])
+    for num in number[1:]:
+        while len(stack) > 0 and stack[-1] < num and k > 0:
+            k -= 1
+            stack.pop()
+        stack.append(num)
 
-    print(answer)
-    return answer
+    if k != 0:
+        stack = stack[:-k]
+    return ''.join(stack)
 
 
 solution("1119"	, 3)
