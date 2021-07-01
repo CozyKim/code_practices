a3de5ebe1f39c207efebe852782ad37ba48b050f
jaehyun <robot032195@gmail.com>
2021-07-02 00:04:55 +0900

    210702

--- BOJ/5430.py
+++ BOJ/5430.py
@@ -1,33 +1,5 @@
 # https://www.acmicpc.net/problem/5430
 
-# import sys
-# from collections import deque
-
-# input = sys.stdin.readline
-
-# T = int(input())
-# for _ in range(T):
-#     p = input().rstrip()
-#     n = int(input())
-#     tmp = input().rstrip().rstrip(']').lstrip('[').split(',')
-#     arr = deque([int(i) for i in tmp if i != ''])
-#     flag = 1
-
-#     if len(arr) < p.count('D'):
-#         print('error')
-#         continue
-#     for op in p:
-#         if op == 'R':
-#             flag *= -1
-#         elif op == 'D' and flag == 1:
-#             arr.popleft()
-#         elif op == 'D' and flag == -1:
-#             arr.pop()
-#     if flag == 1:
-#         print(''.join(str(list(arr)).split()))
-#     else:
-#         print(''.join(str(list(reversed(arr))).split()))
-
 
 import sys
 from collections import deque
