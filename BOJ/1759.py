bccf9f0ca2a72cd206e6eb13da3b331497205b64
jaehyun <robot032195@gmail.com>
2021-07-06 19:38:54 +0900

    210706

---
+++ BOJ/1759.py
@@ -0,0 +1,18 @@
+# https://www.acmicpc.net/problem/1759
+from itertools import combinations
+from collections import Counter
+
+
+L, C = map(int, input().split())
+chars = list(input().split())
+# exception_list = list(combinations(chars, L))
+chars.sort()
+exception_list = list(map(''.join, list(combinations(chars, L))))
+exception_list.sort()
+for cha in exception_list:
+    cnt_vow = 0
+    for c in cha:
+        if c in 'aeiou':
+            cnt_vow += 1
+    if len(cha) - cnt_vow >= 2 and cnt_vow >= 1:
+        print(cha)
