bbb7ae1882142af191684db3ce31e23660d1ab17
jaehyun <robot032195@gmail.com>
2021-12-28 16:28:55 +0900

    211228

---
+++ BOJ/graph/1697.py
@@ -0,0 +1,50 @@
+# https://www.acmicpc.net/problem/1697
+"""
+문제 설명
+숨바꼭질 중이다.
+수빈이는 점 N에 있고 동생은 점 K에 있다. 
+수빈이는 걷거나 순간이동 가능
+수빈이의 위치가 X일 때 걷는다면 1초 후에 X-1 또는 X+1 로 이동하게 된다
+순간이동을 하는 경우 1초 후 2*X의 위치로 이동
+수빈이가 동생을 찾을 수 있는 가장 빠른 시간이 몇 초 후인가?
+"""
+from collections import deque
+
+# 수빈이의 위치 N, 동생의 위치 K
+N, K = map(int, input().split())
+
+# 이미 지나간 곳
+visited = [0 for _ in range(min(2 * max(N, K) + 1, 150001))]
+visited[N] = 1
+
+
+def bfs():
+    q = deque()
+    CNT = 0
+    q.append((N, CNT))
+    if N == K:
+        return 0
+    while q:
+        pos, cnt = q.popleft()
+
+        next1, next2, next3 = pos + 1, pos - 1, pos * 2
+
+        if next1 == K or next2 == K or next3 == K:
+            return cnt + 1
+
+        # 다음 위치가 visited 범위를 넘어서선 안된다.
+        if 0 <= next1 <= len(visited) - 1:
+            if not visited[next1]:
+                visited[next1] = 1
+                q.append((next1, cnt + 1))
+        if 0 <= next2 <= len(visited) - 1:
+            if not visited[next2]:
+                visited[next2] = 1
+                q.append((next2, cnt + 1))
+        if 0 <= next3 <= len(visited) - 1:
+            if not visited[next3]:
+                visited[next3] = 1
+                q.append((next3, cnt + 1))
+
+
+print(bfs())
