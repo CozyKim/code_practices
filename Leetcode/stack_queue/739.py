4c12ff661139695cfa00321d43ad000e3522c3f6
jaehyun <robot032195@gmail.com>
2022-01-30 23:10:57 +0900

    220130

--- Leetcode/stack_queue/739.py
+++ Leetcode/stack_queue/739.py
@@ -17,6 +17,22 @@ class Solution:
                 stack.append(idx)
         return result
 
+    # 책에서 제안한 풀이
+    def dailyTemperatures(self, temperatures):
+        answer = [0] * len(temperatures)
+        stack = []
+        for i, cur in enumerate(temperatures):
+            # 현재 온도가 스택 값보다 높다면 정답 처리
+            while stack and cur > temperatures[stack[-1]]:
+                last = stack.pop()
+                answer[last] = i - last
+            stack.append(i)
+
+        return answer
+
+
+# .... index로 처리했기 때문에 dict가 필요 없었다..
+
 
 sol = Solution()
 sol.dailyTemperatures([73, 74, 75, 71, 69, 72, 76, 73])
