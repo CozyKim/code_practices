0b66aa3fc2d6ca78642cde313e4c1399d8af582e
jaehyun <robot032195@gmail.com>
2022-03-23 17:43:36 +0900

    220323

---
+++ Programmers_CodeTest/lv3_graph_ranking.py
@@ -0,0 +1,44 @@
+# https://programmers.co.kr/learn/courses/30/lessons/49191
+
+from collections import defaultdict
+
+
+def solution(n, results):
+    answer = 0
+    win_graph = defaultdict(list)
+    lose_graph = defaultdict(list)
+    for w, l in results:
+        win_graph[l].append(w)
+        lose_graph[w].append(l)
+
+    wins = set()
+    loses = set()
+
+    def dfs(node, win):
+        if win:
+            if node in wins:
+                return
+            wins.add(node)
+            for next_node in win_graph[node]:
+                if next_node not in wins:
+                    dfs(next_node, win)
+        else:
+            if node in loses:
+                return
+            loses.add(node)
+            for next_node in lose_graph[node]:
+                if next_node not in loses:
+                    dfs(next_node, win)
+
+    for i in range(1, n + 1):
+        dfs(i, True)
+        dfs(i, False)
+        if len(wins) + len(loses) - 2 == n - 1:
+            print(i)
+            answer += 1
+        wins = set()
+        loses = set()
+    return answer
+
+
+print(solution(5, [[4, 3], [4, 2], [3, 2], [1, 2], [2, 5]]))
